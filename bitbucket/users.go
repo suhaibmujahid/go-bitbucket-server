5e2b1a7f0b77e3b0f0e372dff3f661ae9914492c
Suhaib Mujahid <suhaibmujahid@gmail.com>
2020-03-22 16:59:17 -0400

    Implementation to retrieve a list of users with filtering ability

--- bitbucket/users.go
+++ bitbucket/users.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"github.com/google/go-querystring/query"
+	"net/url"
 )
 
 // UsersService handles communication with the user related
@@ -70,4 +72,84 @@ func (s *UsersService) Get(ctx context.Context, slug string) (*User, *Response, 
 	}
 
 	return user, resp, nil
+}
+
+type ListUsersPermissions []ListUsersPermission
+
+func (l ListUsersPermissions) EncodeValues(key string, v *url.Values) error {
+	if len(l) == 0 {
+		return nil
+	}
+
+	qu, err := query.Values(l[0])
+	if err != nil {
+		return err
+	}
+	v.Add(key, qu.Get(key))
+	qu.Del(key)
+	appendOptions(key, v, qu)
+
+	for i := 1; i < len(l); i++ {
+		qu, err := query.Values(l[i])
+		if err != nil {
+			return err
+		}
+		v.Add(fmt.Sprintf("%s.%d", key, i), qu.Get(key))
+		qu.Del(key)
+		appendOptions(fmt.Sprintf("%s.%d", key, i), v, qu)
+	}
+
+	return nil
+}
+
+func appendOptions(prefix string, v *url.Values, new url.Values) {
+	for name := range new {
+		v.Add(fmt.Sprintf("%s.%s", prefix, name), new.Get(name))
+	}
+}
+
+type ListUsersOptions struct {
+	// Filter return only users, whose username, name or email address contain this value.
+	Filter string `url:"filter,omitempty"`
+
+	// Group return only users who are members of the given group.
+	Group string `url:"group,omitempty"`
+
+	// Permission return users who have the specified permissions.
+	Permission ListUsersPermissions `url:"permission,omitempty"`
+
+	ListOptions
+}
+
+type ListUsersPermission struct {
+	Permission     string `url:"permission"`
+	ProjectKey     string `url:"projectKey,omitempty"`
+	RepositorySlug string `url:"repositorySlug,omitempty"`
+	RepositoryId   string `url:"repositoryId,omitempty"`
+}
+
+// List retrieves a page of users, optionally run through provided filters.
+//
+// Bitbucket Server API doc: https://docs.atlassian.com/bitbucket-server/rest/7.0.1/bitbucket-rest.html#idp401
+func (s *UsersService) List(ctx context.Context, opts *ListUsersOptions) ([]*User, *Response, error) {
+	u, err := addOptions("users", opts)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	req, err := s.client.NewRequest("GET", u, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	var users []*User
+	page := &pagedResponse{
+		Values: &users,
+	}
+	resp, err := s.client.Do(ctx, req, page)
+	if err != nil {
+		return nil, resp, err
+	}
+
+	return users, resp, nil
 }
